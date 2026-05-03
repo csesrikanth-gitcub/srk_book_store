6912ed3ac0daa963fed86594a5d6bcb0375b9fcc
rames <rames@host.docker.internal>
2026-05-03 17:22:08 +0530

    updated logger

--- demo_one/src/main/java/com/example/practice/demo_one/Controller/BookController.java
+++ demo_one/src/main/java/com/example/practice/demo_one/Controller/BookController.java
@@ -32,7 +32,7 @@ public class BookController {
 	@GetMapping("/all")
 	public ResponseEntity<?> getBooks()
 	{
-		System.out.println("Entered into controller and trying to Getting the books");
+		logger.info("Entered into controller and trying to Getting the books");
 		
 		List<Book> bookList=bservice.getAllBooks();
 		
